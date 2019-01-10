f8f18fdf46832c04002f616d13bcc514b35fc1fd
Serina Tan <serinatan@cs.utoronto.edu>
2019-01-10 18:14:26 -0500

    add build scripts

---
+++ scripts/compile/compile.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+source ../config
+
+export CUDA_BIN_PATH=$CUDAHOME
+
+case $1 in 
+    "clean")
+        case $2 in 
+            "parboil")
+                cd $ROOT/benchmarks/parboil
+                source clean.sh
+                ;;
+                
+            "cutlass")
+                cd $ROOT/benchmarks/cutlass
+                if [ ! -d "build/" ]; then
+                    mkdir build && cd build 
+                    cmake ..
+                else
+                    cd build
+                fi
+
+                make clean
+
+                ;;
+            "cuda-sdk")
+                cd $ROOT/benchmarks/cuda-sdk
+
+                for dir in ./*
+                do
+                    cd $dir
+                    make clean
+                    cd ..
+                done
+                ;;
+            *)
+                echo ">>>>>>>>>>>>>>>>> Benchmark unknown! Exiting... <<<<<<<<<<<<<<<<"
+                echo "Usage: ./build [compile | clean] [parboil | cutlass | cuda-sdk]"
+                exit
+                ;;
+        esac
+ 
+        ;;
+    "compile")
+        case $2 in 
+            "parboil")
+                cd $ROOT/benchmarks/parboil
+                source compile.sh
+                ;;
+                
+            "cutlass")
+                cd $ROOT/benchmarks/cutlass
+                if [ ! -d "build/" ]; then
+                    mkdir build && cd build 
+                    cmake ..
+                else
+                    cd build
+                fi
+
+                make -j12
+
+                ;;
+            "cuda-sdk")
+                cd $ROOT/benchmarks/cuda-sdk
+
+                for dir in ./*
+                do
+                    echo $dir
+                    cd $dir
+                    make -j12
+                    cd ..
+                done
+                ;;
+            *)
+                echo ">>>>>>>>>>>>>>>>> Benchmark unknown! Exiting... <<<<<<<<<<<<<<<<"
+                echo "Usage: ./build [compile | clean] [parboil | cutlass | cuda-sdk]"
+                exit
+                ;;
+        esac
+        ;;
+    *)
+        echo ">>>>>>>>>>>>>>> Option unknown! Exiting... <<<<<<<<<<<<<<"
+        echo "Usage: ./build [compile | clean] [parboil | cutlass | cuda-sdk]"
+        exit
+        ;;
+esac
+
+
+
+
+         
