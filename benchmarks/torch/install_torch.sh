6f9d6cdc56d78f9637361d1086e7e0af2aad48ee
Serina <serinatan@cs.toronto.edu>
2019-05-29 16:15:59 -0400

    pip install torch

---
+++ benchmarks/torch/install_torch.sh
@@ -0,0 +1,2 @@
+pip3 install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp36-cp36m-linux_x86_64.whl
+pip3 install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp36-cp36m-linux_x86_64.whl
