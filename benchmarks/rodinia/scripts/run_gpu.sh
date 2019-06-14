53db26ac0a592772f27b1899b32944cb29d75509
Serina Tan <serinatan@cs.toronto.edu>
2019-06-14 12:35:23 -0400

    rodinia run script update with 3 sections

--- benchmarks/rodinia/scripts/run_gpu.sh
+++ benchmarks/rodinia/scripts/run_gpu.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+    echo "run_gpu.sh [outdir]"
+    exit
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OCLDIR=$DIR/../cuda
+nsight="/usr/local/cuda/NsightCompute-2019.3/nv-nsight-cu-cli"
+nvprof="/usr/local/cuda/bin/nvprof"
 
 # 10 in total
 #  and  does not work
@@ -9,14 +17,19 @@ bm="backprop b+tree dwt2d heartwall hotspot3D kmeans hybridsort \
     leukocyte \
     nw pathfinder streamcluster bfs gaussian hotspot \
     lavaMD lud nn particlefilter srad huffman"
+#bm="particlefilter"
 
-OUTDIR=$DIR/results-utilsust
+#OUTDIR=$DIR/results-utilsust
 #mkdir -f $OUTDIR &>/dev/null
-mkdir -p $OUTDIR 
+OUTDIR=$DIR/$1
+mkdir -p $OUTDIR/comp
+mkdir -p $OUTDIR/inst
+mkdir -p $OUTDIR/mem
+mkdir -p $OUTDIR/time
 
 cd $OCLDIR
-exe() { echo "++ $@" |& tee -a $OUTDIR/$b.txt ; \
-    $@ |& tee -a $OUTDIR/$b.txt ; }
+#exe() { echo "++ $@" |& tee -a $OUTDIR/$b.txt ; \
+    #$@ |& tee -a $OUTDIR/$b.txt ; }
 
 for b in $bm; do
     echo -n > $OUTDIR/$b.txt # clean output file
@@ -29,10 +42,11 @@ for b in $bm; do
     #    exe echo
     #done
     cmd=$(cat run)
-    #nv-nsight-cu-cli -f --section-folder ../sections/ --section InstCounter --csv $cmd | tee $OUTDIR/$b.txt
-    nv-nsight-cu-cli -f --section-folder ../sections/ --section PertSustainedActive --csv $cmd | tee $OUTDIR/$b.txt
-    #nv-nsight-cu-cli -f --section-folder ../sections/ --section PertBurstActive --csv $cmd | tee $OUTDIR/$b.txt
+    #$nsight -f --section-folder $DIR/../../sections/ --section PertSustainedActive --csv $cmd | tee $OUTDIR/comp/$b.txt
+    #$nsight -f --section-folder $DIR/../../sections/ --section Memory_Usage --csv $cmd | tee $OUTDIR/mem/$b.txt
+    #$nsight -f --section-folder $DIR/../../sections/ --section InstCounter --csv $cmd | tee $OUTDIR/inst/$b.txt
 
+    $nvprof -f --print-gpu-trace --csv --normalized-time-unit ms --log-file $OUTDIR/time/$b.txt $cmd
     cd $OCLDIR
     #exe echo
     #echo
