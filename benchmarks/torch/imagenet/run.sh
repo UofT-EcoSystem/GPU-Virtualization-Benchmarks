0a6e4f3e1b3bc023b988261ef83f2c948b3dfaf6
Serina Tan <serinatan@cs.toronto.edu>
2019-06-14 12:52:52 -0400

    remove torch impl of ML

--- benchmarks/torch/imagenet/run.sh
+++
@@ -1,6 +0,0 @@
-#/usr/local/NVIDIA-Nsight-Compute-2019.3/nv-nsight-cu-cli -o resnet50 --target-processes all --section-folder ../../sections --section PertSustainedActive -f --profile-from-start off --csv ./main.py -a resnet50 --pretrained /imagenet -b 32 --gpu 0 --profile 5 --epochs 1
-#/usr/local/NVIDIA-Nsight-Compute-2019.3/nv-nsight-cu-cli -o resnet50-inst --target-processes all \ 
-#--section-folder ../../sections --section InstCounter -f --profile-from-start off \ 
-#--csv /opt/conda/bin/python main.py -a resnet50 --pretrained /dataset -b 32 --gpu 2 \
-#--profile 5 --epochs 1
-/usr/bin/python main.py -a resnet50 --pretrained /dataset -b 32 --gpu 2 --profile 5 --epochs 1
--- benchmarks/torch/install_torch.sh
+++
@@ -1,2 +0,0 @@
-pip3 install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp36-cp36m-linux_x86_64.whl
-pip3 install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp36-cp36m-linux_x86_64.whl
