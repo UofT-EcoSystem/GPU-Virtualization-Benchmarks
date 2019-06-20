9d46f1a8210a3bbe18cc71fa87242d088b228e9d
Serina <serinatan@cs.toronto.edu>
2019-06-19 22:21:54 -0400

    arioc run script

--- benchmarks/arioc/scripts/run.sh
+++ benchmarks/arioc/scripts/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-if [ "$#" -ne 1 ]; then
+# usage: ./run.sh [1|2|3] [inst|comp|mem|time] GPU_ID
+if [ "$#" -ne 3 ]; then
     echo "Illegal number of parameters"
-    echo "run.sh [1|2|3]"
+    echo "run.sh [1|2|3] [inst|comp|mem|time] GPU_ID"
     exit
 fi
 
@@ -17,6 +17,8 @@ comp_sec="PertSustainedActive"
 inst_sec="InstCounter"
 mem_sec="Memory_Usage"
 
+
+export CUDA_VISIBLE_DEVICES=$3
 
 if [ $1 == "1" ]; then
   # run stage 1
@@ -35,18 +37,19 @@ else
   cd $ROOT/A/S_cerevisiae
   pwd
 
-  $nvprof --csv -f --print-gpu-trace --normalized-time-unit ms \
-    --log-file $ROOT/profile/time.txt $ROOT/bin/AriocP AriocP.paired.cfg 
-
-  $nsight --csv -f --section-folder $sec_folder --section $comp_sec \
-    $ROOT/bin/AriocP AriocP.paired.cfg | tee $ROOT/profile/comp.txt
-
-  $nsight --csv -f --section-folder $sec_folder --section $inst_sec \
-    $ROOT/bin/AriocP AriocP.paired.cfg | tee $ROOT/profile/inst.txt
-
-  $nsight --csv -f --section-folder $sec_folder --section $mem_sec \
-    $ROOT/bin/AriocP AriocP.paired.cfg | tee $ROOT/profile/mem.txt
-
+  if [ $2 == "time" ]; then
+    $nvprof --csv -f --print-gpu-trace --normalized-time-unit ms \
+      --log-file $ROOT/profile/time.txt $ROOT/bin/AriocP AriocP.paired.cfg 
+  elif [ $2 == "comp" ]; then
+    $nsight --csv -f --section-folder $sec_folder -s 1580 --section $comp_sec \
+      $ROOT/bin/AriocP AriocP.paired.cfg | tee $ROOT/profile/comp.txt
+  elif [ $2 == "inst" ]; then
+    $nsight --csv -f --section-folder $sec_folder -s 1580 --section $inst_sec \
+      $ROOT/bin/AriocP AriocP.paired.cfg | tee $ROOT/profile/inst.txt
+  elif [ $2 == "mem" ]; then
+    $nsight --csv -f --section-folder $sec_folder -s 1580 --section $mem_sec \
+      $ROOT/bin/AriocP AriocP.paired.cfg | tee $ROOT/profile/mem.txt
+  fi
 
 fi
 
