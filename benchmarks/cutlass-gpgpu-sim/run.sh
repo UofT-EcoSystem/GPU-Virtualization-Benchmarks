0454958d6d3be10b39c2e4ad12a36de133b8f780
Serina Tan <serinatan@cs.toronto.edu>
2019-07-25 17:35:01 -0400

    run script for simulator

--- benchmarks/cutlass-gpgpu-sim/run.sh
+++ benchmarks/cutlass-gpgpu-sim/run.sh
@@ -2,26 +2,37 @@
 
 # run.sh dgemm|wmma|sgemm|igemm nsight|sim
 
-NSIGHT=/usr/local/cuda-10.1/NsightCompute-2019.3/nv-nsight-cu-cli
+NSIGHT=/usr/local/cuda/NsightCompute-2019.3/nv-nsight-cu-cli
 SECTION_FOLDER=/home/serinatan/project/GPU-Virtualization-Benchmarks/benchmarks/sections
+NVPROF=/usr/local/cuda/bin/nvprof
 
 if [ $2 == "nsight" ]; then
-  PREFIX="$NSIGHT --section-folder $SECTION_FOLDER --section InstCounter --csv"
+  PREFIX="$NSIGHT --section-folder $SECTION_FOLDER --section InstCounter --csv -f"
+elif [ $2 == "nvprof" ]; then
+  PREFIX="$NVPROF --print-gpu-trace --csv -f "
 else
   PREFIX=""
 fi
 
 if [ $1 == "dgemm" ]; then
   make clean
-  make CFLAGS=-DDGEMM_3
-  $PREFIX ./cutlass-test | tee profile/dgemm3.txt
+  make CFLAGS=-DDGEMM_4
+  $PREFIX ./cutlass-test | tee profile/dgemm4$2.txt
 elif [ $1 == "wmma" ]; then
   make clean
   make CFLAGS=-DWMMA_36
-  $PREFIX ./cutlass-test | tee profile/wmma36.txt
+  $PREFIX ./cutlass-test | tee profile/wmma36$2.txt
 elif [ $1 == "sgemm" ]; then
   make clean
-  make CFLAGS=-DSGEMM_3
-  $PREFIX ./cutlass-test | tee profile/sgemm3.txt
+  make CFLAGS=-DSGEMM_4
+  $PREFIX ./cutlass-test | tee profile/sgemm4$2.txt
+elif [ $1 == "igemm" ]; then
+  make clean
+  make CFLAGS=-DIGEMM_1
+  $PREFIX ./cutlass-test | tee profile/igemm1$2.txt
+elif [ $1 == "hgemm" ]; then
+  make clean
+  make CFLAGS=-DHGEMM_1
+  $PREFIX ./cutlass-test | tee profile/hgemm1$2.txt
 fi
 
