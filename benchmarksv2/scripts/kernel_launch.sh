115a1b0e2d503a858c0e66c209ffc3d51ff10e51
Serina Tan <serinatan@cs.toronto.edu>
2019-08-18 14:24:34 -0400

    script to run sgemm and stencil

---
+++ benchmarksv2/scripts/kernel_launch.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+if [ "$#" -lt 2 ]; then
+  echo "Illegal number of parameters"
+  echo "Usage: run.sh <benchmark[-benchmark...]> <sim|hw> [gdb|nsight|nvprof]"
+  echo "nsight/nvprof is only available for hw mode."
+fi
+
+
+BENCH_ROOT="$(dirname "$(pwd)")"
+PARBOIL_ROOT="$BENCH_ROOT/parboil"
+PARBOIL_DATA=$PARBOIL_ROOT/datasets
+NSIGHT_SECTION=$PARBOIL_ROOT/../sections
+gpgpusim=/mnt/ecosystem-gpgpu-sim/
+
+# define benchmark to dataset pair (largest available set)
+#declare -A datamap=(["cutcp"]="large" ["histo"]="large" ["lbm"]="long" ["mri_gridding"]="small" \
+#  ["mri_q"]="large" ["sad"]="large" ["sgemm"]="medium" ["spmv"]="large" ["stencil"]="default" \
+#  ["tpacf"]="large")
+
+
+declare -A datamap=(["cutcp"]="large" ["sgemm"]="medium" ["tpacf"]="large" ["lbm"]="long" ["sad"]="large" ["spmv"]="large" ["stencil"]="default") 
+
+PROFILE=""
+
+
+if [ "$2" == "sim" ]; then
+  PARBOIL_BUILD=$PARBOIL_ROOT/build-docker
+  if [ "$3" == "gdb" ]; then
+    PROFILE="gdb --args "
+  fi
+ 
+else
+  PARBOIL_BUILD=$PARBOIL_ROOT/build
+
+  if [ "$3" == "nsight" ]; then
+    #PROFILE="nv-nsight-cu-cli -f --csv --section-folder $NSIGHT_SECTION --section Memory_Usage "
+    PROFILE="nv-nsight-cu-cli -f --csv "
+  elif [ "$3" == "nvprof" ]; then
+    PROFILE="/usr/local/cuda-10.0/bin/nvprof -f -o conc.prof "
+  fi
+fi
+
+
+
+#function param: $1=benchmark $2=dataset
+function build_input() {
+  folder=$PARBOIL_DATA/$1/$2/input
+
+  IO="$1 "
+
+  # grab input params from description file
+  if [ -f "$folder/DESCRIPTION" ]; then
+    while IFS='\n' read -r line; do
+      # split key values
+      IFS=':' read -ra words <<< "$line"
+
+      if [ "${words[0]}" == "Inputs" ]; then
+        IO="$IO-i "
+        # split inputs
+        IFS=' ' read -ra inputs <<< "${words[1]}"
+
+        empty=true
+        for i in ${inputs[@]}; do 
+          if [ "$empty" = false ]; then
+            IO="$IO,"
+          fi
+
+          IO="$IO$folder/$i"
+
+          empty=false
+        done
+
+        IO="$IO -o $PARBOIL_BUILD/out/$1/$1_$2.out"
+      fi
+
+
+      if [ "${words[0]}" == "Parameters" ]; then
+        IO="$IO ${words[1]}"
+      fi
+
+    done < "$folder/DESCRIPTION"
+
+  else
+    IO="$IO-i $folder/$(ls $folder) -o $PARBOIL_BUILD/out/$1/$1_$2.out"
+  fi
+
+
+  mkdir -p $PARBOIL_BUILD/out/$1
+  
+  echo "$IO"
+}
+
+# make results folder if none exists
+#mkdir -p results
+
+IFS='-' read -ra benchmarks <<< "$1"
+
+# make a new build folder for this run
+mkdir -p $BENCH_ROOT/build
+rm -rf $BENCH_ROOT/build/$1
+mkdir -p $BENCH_ROOT/build/$1
+
+inputs=""
+defines=""
+for bench in ${benchmarks[@]}; do
+  if [[ -v "datamap[$bench]" ]]; then
+    echo ">>>>>>>>> Running kernel: $bench <<<<<<<<<"
+  else
+    echo "Benchmarks do not exist: $bench"
+    exit
+  fi
+
+  # make input files
+  IO=$(build_input "$bench" "${datamap[$bench]}")
+  txt=$BENCH_ROOT/build/$1/$bench.txt
+  echo "$IO" > $txt
+
+  inputs="$inputs$txt "
+  defines="$defines-D$bench=ON "
+done
+
+# compile a new build!
+cd $BENCH_ROOT/build/$1 && cmake $defines $BENCH_ROOT && make -j all
+
+if [ "$2" == "sim" ]; then
+  # manually relink the executable with shared runtime
+  cp $BENCH_ROOT/scripts/link.sh .
+  ./link.sh
+
+  # copy gpgpu-sim config
+  cp $BENCH_ROOT/scripts/gpgpusim.config .
+  cp $BENCH_ROOT/scripts/config_volta_islip.icnt .
+
+  # source gpgpusim setup scripts
+  (cd $gpgpusim && source setup_environment)
+fi
+
+mkdir -p $BENCH_ROOT/results
+
+if [ ${#benchmarks[@]} -eq 1 ]; then
+  mkdir -p $BENCH_ROOT/results/seq
+  res_folder=$BENCH_ROOT/results/seq
+else
+  mkdir -p $BENCH_ROOT/results/conc
+  res_folder=$BENCH_ROOT/results/conc
+fi
+
+$PROFILE ./driver $inputs | tee $res_folder/$1.txt
+
+
+
