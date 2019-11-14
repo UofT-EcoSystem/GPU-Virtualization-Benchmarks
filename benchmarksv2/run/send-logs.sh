a2fd542907e160c9f582031fe4bab49b1eaff35f
Serina <serinatan@cs.toronto.edu>
2019-11-14 18:55:07 -0500

    rsync cmd to copy logs onto serina's machine

---
+++ benchmarksv2/run/send-logs.sh
@@ -0,0 +1,3 @@
+rsync -a --prune-empty-dirs --include '*/' --include="*.o*" --exclude '*' run-isolation-intra me:/media/hdisk/home/serina/gpusim
+scp logfiles/* me:/media/hdisk/home/serina/gpusim/logfiles
+
