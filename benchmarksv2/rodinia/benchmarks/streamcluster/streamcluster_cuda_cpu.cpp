028630da3fca28b493be9549fd7bc51e90c07d07
serinatan <serinatan@cs.toronto.edu>
2020-01-21 15:09:12 -0500

    FIX: typo in streamcluster

--- benchmarksv2/rodinia/benchmarks/streamcluster/streamcluster_cuda_cpu.cpp
+++ benchmarksv2/rodinia/benchmarks/streamcluster/streamcluster_cuda_cpu.cpp
@@ -365,7 +365,7 @@ float pFL(Points *points, int *feasible, int numfeasible,
 	    change += pgain(feasible[x], points, z, k, kmax, is_center, center_table, switch_membership, isCoordChanged,
 						&serial_t, &cpu_to_gpu_t, &gpu_to_cpu_t, &alloc_t, &kernel_t, &free_t);
 
-	    cudaStreamSynchronize(streamcluster::stream);
+	    cudaStreamSynchronize(streamcluster::gpusim_stream);
 
 	    can_exit = set_and_check(streamcluster::gpusim_uid, false);
     }
