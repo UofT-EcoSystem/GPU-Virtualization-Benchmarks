f83df0d5cf621074e14ef612361f6513751852b3
Serina Tan <serinatan@cs.toronto.edu>
2020-03-11 14:46:47 -0400

    Update prediction features; avg error for 5-fold CV is about 0.02

--- util/data/scripts/predict/predict_slowdown.py
+++ util/data/scripts/predict/predict_slowdown.py
@@ -13,17 +13,33 @@ import os
 import data.scripts.common.constants as const
 
 cols_prefix = ['norm_ipc',
-               # 'avg_rbh',
-               'diff_rbh',
+               'avg_rbh',
+               'MPKI',
+               'ratio_rbh',
                'diff_mflat',
-               'diff_bw',
-               'diff_mpc',
-               'diff_thread',
+               'ratio_mpc',
+               'ratio_bw',
                'sum_bw',
-               # 'l2_miss_rate',
-               # 'avg_dram_bw',
-               # 'avg_mem_lat',
+               'ratio_thread',
+               'ratio_inst_empty',
+               'sum_inst_empty',
+               # 'ratio_sp_busy',
+               # 'sum_sp_busy',
+               'ratio_int_busy',
+               'sum_int_busy',
+               'diff_ipc',
+               'l2_miss_rate',
                ]
+
+cols_ws = ['sum_norm_ipc',
+           'sum_avg_rbh',
+           'sum_mflat',
+           'sum_mpc',
+           'sum_bw',
+           'sum_inst_empty',
+           'sum_int_busy',
+           'sum_sp_busy',
+           ]
 
 
 def export_tree(clf, path_png, tree_id=300):
@@ -43,7 +59,7 @@ def export_tree(clf, path_png, tree_id=300):
         f.write(png_bytes)
 
 
-def prepare_datasets(df_pair, cc):
+def prepare_datasets(df_pair):
 
     def feature_set(sfx_base, sfx_other):
         def calculate_diff(derived_metric, metric):
@@ -51,18 +67,39 @@ def prepare_datasets(df_pair, cc):
                 (df_pair[metric+'_'+sfx_base] - df_pair[metric+'_'+sfx_other]) \
                 / df_pair[metric+'_'+sfx_base]
 
-        calculate_diff('diff_mflat', 'avg_mem_lat')
-        calculate_diff('diff_bw', 'avg_dram_bw')
-        calculate_diff('diff_mpc', 'mpc')
-        calculate_diff('diff_thread', 'thread_count')
-        calculate_diff('diff_rbh', 'avg_rbh')
+        def calculate_ratio(derived_metric, metric):
+            df_pair[derived_metric+'_'+sfx_base] = \
+                    df_pair[metric+'_'+sfx_base] / df_pair[metric+'_'+sfx_other]
 
-        df_pair['sum_bw_'+sfx_base] = df_pair['avg_dram_bw_x'] + df_pair[
-            'avg_dram_bw_y']
+        def calculate_sum(derived_metric, metric):
+            df_pair[derived_metric+'_'+sfx_base] = \
+                    df_pair[metric+'_'+sfx_base] + df_pair[metric+'_'+sfx_other]
+
+        calculate_diff('diff_mflat', 'avg_mem_lat')
+        calculate_diff('diff_ipc', 'ipc')
+        calculate_ratio('ratio_mpc', 'mpc')
+        calculate_ratio('ratio_rbh', 'avg_rbh')
+
+        calculate_ratio('ratio_bw', 'avg_dram_bw')
+        calculate_sum('sum_bw', 'avg_dram_bw')
+
+        calculate_ratio('ratio_sp_busy', 'sp_busy')
+        calculate_sum('sum_sp_busy', 'sp_busy')
+
+        calculate_ratio('ratio_int_busy', 'int_busy')
+        calculate_sum('sum_int_busy', 'int_busy')
+
+        # calculate_ratio('ratio_int_cycles', 'stall_int_cycles')
+        # calculate_sum('sum_int_cycles', 'stall_int_cycles')
+
+        calculate_ratio('ratio_thread', 'thread_count')
+        
+        calculate_ratio('ratio_inst_empty', 'inst_empty_cycles')
+        calculate_sum('sum_inst_empty', 'inst_empty_cycles')
 
         cols = [c + '_' + sfx_base for c in cols_prefix]
         X = df_pair[cols].values
-        X = X.astype(np.float32)
+        X = X.astype(np.double)
 
         sld_col = '1_sld' if sfx_base == 'x' else '2_sld'
         y = df_pair[sld_col]
@@ -71,7 +108,7 @@ def prepare_datasets(df_pair, cc):
         print('y invalid?', np.isnan(y).any())
 
         # replace inf and -inf
-        X = np.nan_to_num(X, neginf=-1e30)
+        X = np.nan_to_num(X, neginf=-1e30, posinf=1e30)
 
         return X, y
 
@@ -88,12 +125,42 @@ def prepare_datasets(df_pair, cc):
     return X, y
 
 
+def prepare_ws_datasets(df_pair):
+    def calculate_sum(derived_metric, metric):
+        df_pair[derived_metric] = \
+            df_pair[metric + '_x'] + df_pair[metric + '_y']
+
+    calculate_sum('sum_norm_ipc', 'norm_ipc')
+    calculate_sum('sum_avg_rbh', 'avg_rbh')
+    calculate_sum('sum_mflat', 'avg_mem_lat')
+    calculate_sum('sum_mpc', 'mpc')
+    calculate_sum('sum_bw', 'avg_dram_bw')
+    calculate_sum('sum_inst_empty', 'inst_empty_cycles')
+    calculate_sum('sum_int_busy', 'int_busy')
+    calculate_sum('sum_sp_busy', 'sp_busy')
+
+    X = df_pair[cols_ws].values
+    X = X.astype(np.double)
+
+    y = df_pair['ws']
+
+    print('X invalid?', np.isnan(X).any())
+    print('y invalid?', np.isnan(y).any())
+
+    # replace inf and -inf
+    X = np.nan_to_num(X, neginf=-1e30, posinf=1e30)
+
+    X, y = shuffle(X, y, random_state=5)
+
+    return X, y
+
+
 def train(X, y):
     # #########################################################################
     # Fit gradient boost tree regression model using K-fold cross validation
-    kf = KFold(n_splits=5)
-    params = {'n_estimators': 500, 'max_depth': 5, 'min_samples_split': 2,
-              'learning_rate': 0.01, 'loss': 'huber'}
+    kf = KFold(n_splits=5, shuffle=True)
+    params = {'n_estimators': 300, 'max_depth': 9, 'min_samples_split': 2,
+              'learning_rate': 0.1, 'loss': 'huber'}
 
     for train_index, test_index in kf.split(X):
         X_train, X_test = X[train_index], X[test_index]
@@ -114,32 +181,39 @@ def train(X, y):
     return clf
 
 
-def plot_importance(clf):
+def plot_importance(clf, type='old'):
+    fig = plt.figure(figsize=(20, 10))
+    plt.style.use('seaborn-talk')
     # #########################################################################
     # Plot feature importance
     feature_importance = clf.feature_importances_
     # make importances relative to max importance
-    feature_importance = 100.0 * (feature_importance / feature_importance.max())
+    # feature_importance = 100.0 * (feature_importance / feature_importance.max())
+    feature_importance = feature_importance
 
     sorted_idx = np.argsort(feature_importance)
     pos = np.arange(sorted_idx.shape[0]) + .5
 
     plt.barh(pos, feature_importance[sorted_idx], align='center')
 
-    np_cols = np.array(cols_prefix)
-    plt.yticks(pos, np_cols[sorted_idx])
-    plt.xlabel('Relative Importance')
+    if type == 'ws':
+        np_cols = np.array(cols_ws)
+    else:
+        np_cols = np.array(cols_prefix)
+
+    plt.yticks(pos, np_cols[sorted_idx], fontsize=20)
+    plt.xlabel('Importance')
     plt.title('Variable Importance')
 
-    plt.show()
-    plt.close()
+    # plt.show()
+    fig.savefig(os.path.join(const.DATA_HOME, 'graphs/grad_boosting_imp.pdf'))
 
 
 def predict_from_df(clf, df_pair, suffix):
     cols = [c + '_' + suffix for c in cols_prefix]
 
-    X = df_pair[cols].values.astype(float)
-    X = np.nan_to_num(X, neginf=-1e30)
+    X = df_pair[cols].values.astype(np.double)
+    X = np.nan_to_num(X, neginf=-1e30, posinf=1e30)
 
     return clf.predict(X)
 
