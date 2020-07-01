c73666b9fe70b00f3e588a9b106688ab174d7991
Serina Tan <serinatam0812@gmail.com>
2020-07-01 15:15:04 -0400

    FIX: only bypass L2D for intra isolation runs

--- util/job_launching/isolation.py
+++ util/job_launching/isolation.py
@@ -102,16 +102,18 @@ for app in args.apps:
                                 )
                        for cfg in configs]
 
-            # Check if we should disable l2d
-            configs = [cfg + "-BYPASS_L2D_S1" if
-                       df_seq_multi.loc[(bench, kidx)]['l2_miss_rate'] >
-                       const.l2d_bypass_threshold
-                       else cfg for cfg in configs]
+            if args.intra:
+                # Check if we should disable l2d
+                configs = [cfg + "-BYPASS_L2D_S1" if
+                           df_seq_multi.loc[(bench, kidx)]['l2_miss_rate'] >
+                           const.l2d_bypass_threshold
+                           else cfg for cfg in configs]
         else:
-            configs = [cfg + "-BYPASS_L2D_S1" if
-                       df_seq.loc[kernel]['l2_miss_rate']
-                       > const.l2d_bypass_threshold
-                       else cfg for cfg in configs]
+            if args.intra:
+                configs = [cfg + "-BYPASS_L2D_S1" if
+                           df_seq.loc[kernel]['l2_miss_rate']
+                           > const.l2d_bypass_threshold
+                           else cfg for cfg in configs]
 
         cmd = ['python3',
                os.path.join(RUN_HOME, 'run_simulations.py'),
