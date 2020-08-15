f8fd269da40114133f58b610118ae0437414960b
Pavel Golikov <paulgolikov@gmail.com>
2020-08-15 19:34:39 -0400

    graph script and changes to batch rematch script

--- util/gpupool/batch_rematch.py
+++ util/gpupool/batch_rematch.py
@@ -7,10 +7,11 @@ min_jobs = 50
 max_jobs = 300
 max_jobs += job_step
 f = open('sensitivity_2.out', 'a')
-f.write('num_jobs, (gpupool_required_gpus, gpupool_violations, gpupool_ws),\
-mig_required_gpus, random_violations, dram_bw_based_violations\n')
+f.write('num_jobs,gpupool_required_gpus,gpupool_violations,gpupool_ws,\
+mig_required_gpus,mig_ws,random_violations,dram_bw_based_violations,dram_ws\n')
+
 for num_jobs in range(min_jobs, max_jobs, job_step):
-    result = []
+    result = ""
     # generate required number of jobs
     batch = BatchJob(rand_seed=0, num_jobs=num_jobs)
     pickle_name = "pickles/BatchJob-" + str((num_jobs // 50) - 1) + "-Three_D-BoostTree-Steady-False.pkl"
@@ -20,10 +21,10 @@ for num_jobs in range(min_jobs, max_jobs, job_step):
     config = GpuPoolConfig(Allocation.Three_D, StageOne.BoostTree, StageTwo.Steady,
             at_least_once=False, accuracy_mode=True, stage2_buffer=0.15)
 
-    gpupool, gpupool_viol, gpupool_ws = batch._max_matching(config, cores=32)
+    gpupool, gpupool_viol, gpupool_ws, ws_list = batch._max_matching(config, cores=32)
 
     # Get baseline #1: MIG results
-    mig = batch.calculate_gpu_count_mig()
+    mig, mig_ws, mig_ws_list = batch.calculate_gpu_count_mig()
 
     # Get baseline #2: Random matching results
     random = batch.calculate_qos_violation_random(gpupool, 32)
@@ -32,8 +33,15 @@ for num_jobs in range(min_jobs, max_jobs, job_step):
     dram_bw_based, ws_list_dram, migrated_count_dram = \
         batch.calculate_qos_viol_dram_bw(gpupool, 32)
 
-    result.append((batch.num_jobs, (gpupool, gpupool_viol.count, gpupool_ws), mig,
-        random.count, dram_bw_based))
-    f.write('%s\n' % result)
+    dram_ws = [float(i) for i in ws_list_dram]
+    dram_ws_sum = sum(dram_ws) / gpupool
+    print(dram_ws_sum)
+
+    result = "{},{},{},{},{},{},{},{},{}".format(num_jobs, gpupool, gpupool_viol.count, gpupool_ws,
+                                           mig, mig_ws,
+                                           random.count,
+                                           dram_bw_based, dram_ws_sum)
+    f.write(result + '\n')
+
 
 f.close()
